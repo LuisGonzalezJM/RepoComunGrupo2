d82364af15b0b727afaa06c6bbea4948361c0ff9
Nicolás <nicolas.marmal.1@iesjulianmarias.es>
2025-02-17 14:00:14 +0100

    Commit de ElNicos de mi rama

--- FicheroBase1.java
+++ FicheroBase1.java
@@ -1,5 +1,6 @@
 //Este es el fichero original que viene creado
 //1.- Línea original de Luis
+//Hola Luis soy Nicolás aka "ElNicos" nada más que aportar//
 
 public class FicheroBase1{
     public static void main(String[] args) {
